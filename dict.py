e013cdc41203af1f8ddd150d7714b271a59d2299
laylotadjibayeva <tojiboyevalaylo919@gmail.com>
2025-11-10 20:28:55 +0500

    Lug'atlar bo'yicha

---
+++ dict.py
@@ -0,0 +1,76 @@
+# # 1 topshiriq
+# otam = {
+#     'ismi': 'Azamat',
+#     'tugilgan_yili': 1981,
+#     'shahri': 'Xorazm',
+#     'kasbi': 'O\'qituvchi'
+# }
+# onam = {
+#     'ismi': 'Dildora',
+#     'tugilgan_yili': 1985,
+#     'shahri': 'Xorazm',
+#     'kasbi': 'Shifokor'
+# }
+# akam = {
+#     'ismi': 'Ali',
+#     'tugilgan_yili': 2004,
+#     'shahri': 'Xorazm',
+#     'kasbi': 'Dasturchi'
+# }
+# singlim = {
+#     'ismi': 'Muhayyo',
+#     'tugilgan_yili': 2008,
+#     'shahri': 'Xorazm',
+#     'kasbi': 'o\'quvchi'
+# }
+
+# print(f"Otamning ismi {otam['ismi']}, {otam['tugilgan_yili']}-yilda, {otam['shahri']} viloyatida tugʻilgan.")
+# print(f"Onamning ismi {onam['ismi']}, {onam['tugilgan_yili']}-yilda, {onam['shahri']} shahrida tugʻilgan.")
+# print(f"akamning ismi {akam['ismi']}, {akam['tugilgan_yili']}-yilda, {akam['shahri']} shahrida tugʻilgan.")
+# print(f"singlim ismi {singlim['ismi']}, {singlim['tugilgan_yili']}-yilda, {singlim['shahri']} shahrida tugʻilgan.")
+
+
+
+
+# 2 topshiriq
+# sevimli_taomlar = {
+#     'otam': 'Palov',
+#     'onam': 'Manti',
+#     'akam': 'Lag\'mon',
+#     'singlim': 'Somsa',
+#     'ukam': 'Kabob'
+# }
+
+# print(f"Otamning sevimli taomi: {sevimli_taomlar['otam']}")
+# print(f"Onamning sevimli taomi: {sevimli_taomlar['onam']}")
+# print(f"Akamning sevimli taomi: {sevimli_taomlar['akam']}")
+# print(f"Singlimning sevimli taomi: {sevimli_taomlar['singlim']}")
+# print(f"Ukamning sevimli taomi: {sevimli_taomlar['ukam']}")
+
+
+
+
+# # 3 topshiriq
+# python_lugati = {
+#     'integer': 'Butun son (musbat, manfiy yoki nol)',
+#     'float': 'O\'nlik son (kasr qismi mavjud son)',
+#     'string': 'Matn (qo\'shtirnoq ichidagi belgilar ketma-ketligi)',
+#     'list': 'Ro\'yxat (tartiblangan, o\'zgaruvchan elementlar to\'plami)',
+#     'dictionary': 'Lug\'at (kalit-qiymat juftliklaridan iborat ma\'lumot turi)',
+#     'if': 'Shart operatori (agar shart rost bo\'lsa, kod blokini bajaradi)',
+#     'else': 'Aks holda operatori (if sharti yolg\'on bo\'lsa, bajariladi)',
+#     'for': 'Siklni boshqarish operatori (ketma-ketlik bo\'ylab takrorlash uchun)',
+#     'function': 'Funksiya (muayyan vazifani bajarish uchun nomlangan kod bloki)'
+# }
+
+# print(f"integer: {python_lugati['integer']}")
+# print(f"dictionary: {python_lugati['dictionary']}")
+# print(f"float: {python_lugati['float']}")
+# print(f"string: {python_lugati['string']}")
+# print(f"list: {python_lugati['list']}")
+# print(f"if: {python_lugati['if']}")
+# print(f"else: {python_lugati['else']}")
+
+
+
+# 4 topshiriq
