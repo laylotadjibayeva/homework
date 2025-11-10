cc46a408040452a4ca7aeee1a5e21180b731c9b4
laylotadjibayeva <tojiboyevalaylo919@gmail.com>
2025-11-10 20:36:23 +0500

    Nesting mavzusi bo'yicha

---
+++ vazifa.py
@@ -0,0 +1,44 @@
+davlatlar = {
+    "O'zbekiston": {
+        "poytaxti": "Toshkent",
+        "Hududi": "448978 kv.km",
+        "Aholisi": "33000000",
+        "Pul birligi": "so'm"
+    },
+    "Rossiya": {
+        "poytaxti": "Moskva",
+        "Hududi": "17098246 kv.km",
+        "Aholisi": "144000000",
+        "Pul birligi": "rubl"
+    },
+    "AQSH": {
+        "poytaxti": "Vashington",
+        "Hududi": "9631418 kv.km",
+        "Aholisi": "327000000",
+        "Pul birligi": "dollar"
+    },
+    "Malayziya": {
+        "poytaxti": "Kuala-Lumpur",
+        "Hududi": "329750 kv.km",
+        "Aholisi": "25000000",
+        "Pul birligi": "ringgit"
+    }
+}
+
+# for davlat_nomi, ma_lumotlar in davlatlar.items():
+#     print(f"\n{davlat_nomi}ning poytaxti: {ma_lumotlar['poytaxti']}")
+#     print(f"Hududi: {ma_lumotlar['Hududi']}")
+#     print(f"Aholisi: {ma_lumotlar['Aholisi']}")
+#     print(f"Pul birligi: {ma_lumotlar['Pul birligi']}")
+
+
+# davlat_nomi = input("Davlat nomini kiriting: ")
+
+# if davlat_nomi in davlatlar:
+#     malumot = davlatlar[davlat_nomi]
+#     print(f"\n{davlat_nomi}ning poytaxti {malumot['poytaxti']}")
+#     print(f"Hududi: {malumot['Hududi']}")
+#     print(f"Aholisi: {malumot['Aholisi']}")
+#     print(f"Pul birligi: {malumot['Pul birligi']}")
+# else:
+#     print("\nBizda bu davlat haqida ma'lumot mavjud emas")
