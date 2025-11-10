0b9a623e3b2f8610a2348a44238786156d8aee3f
laylotadjibayeva <tojiboyevalaylo919@gmail.com>
2025-11-10 20:14:28 +0500

    For, if va else bo'yicha

---
+++ vazifa 4.py
@@ -0,0 +1,54 @@
+# # 1 topshiriq
+# pochtalar = ["user1@gmail.com", "user2yahoo.com", "user3@outlook.com"]
+# for email in pochtalar:
+#     if "@" in email:
+#         print("To‘g‘ri email:", email)
+#     else:
+#         print("Noto‘g‘ri email:", email)
+
+
+# # 3 topshiriq
+# haftalik_haroratlar = , [20, 22, 19, 24, 25, 23, 21]
+
+
+# # 4 topshiriq
+# taomlar = ["Osh", "Shashlik", "Manti", "Lag’mon"]
+# taom = input("Qanday taom xohlaysiz?>>> ")
+
+# for t in taomlar:
+#     if t.lower() == taom:
+#         print("Buyurtmangiz qabul qilindi!")
+#     else:
+#         print("Kechirasiz, bunday taom yo‘q.")
+
+
+# 5 topshiriq
+yoshlar = [16, 21, 17, 30, 25]
+yosh = int(input("Yoshingizni kiriting: "))
+
+if yosh in yoshlar:
+    if yosh >= 18:
+        print("Xush kelibsiz")
+    else:
+        print("Yosh chegarasiga yetmagan")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
