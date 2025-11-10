b9e9b968a7796a6e38e05790c68c2252512721eb
laylotadjibayeva <tojiboyevalaylo919@gmail.com>
2025-11-10 22:58:33 +0500

    if elif else mavzusi bo'yicha

---
+++ if.elif.else.py
@@ -0,0 +1,50 @@
+# # 1 topshiriq
+# foydalanuvchilar = ["ali", "vali", "umar", "dilshod", "malika"]
+
+# login = input("Yangi login tanlang: ")
+# if login in foydalanuvchilar:
+#     print("Login band, yangi login tanlang!")
+# else:
+#     print("Xush kelibsiz,", login + "!")
+
+
+# # 2 topshiriq
+# son = int(input("Istalgan butun son kiriting: "))
+
+# for n in range(2, 11):
+#     if son % n == 0:
+#         print(son, "soni", n, "ga qoldiqsiz bo'linadi")
+
+
+# # 3 topshiriq
+# son = int(input("Juft son kiriting: "))
+
+# if son % 2 == 0:
+#     print("Rahmat!")
+# else:
+#     print("Bu son juft emas.")
+
+
+# # 4 topshiriq
+# yosh = int(input("Yoshingizni kiriting: "))
+
+# if yosh <= 4 or yosh >= 60:
+#     price = 0
+# elif yosh < 18:
+#     price = 10000
+# else:
+#     price = 20000
+
+# print(f"Sizga chipta narxi {price} so'm")
+
+
+# # 5 topshiriq
+# son1 = float(input("Birinchi sonni kiriting: "))
+# son2 = float(input("Ikkinchi sonni kiriting: "))
+
+# if son1 < son2:
+#     print(son1, "<", son2)
+# elif son1 > son2:
+#     print(son1, ">", son2)
+# else:
+#     print(son1, "=", son2)
